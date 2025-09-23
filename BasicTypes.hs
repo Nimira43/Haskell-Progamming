ce42908f839cbcbc38f8581b656d4543186632f0
Nimira43 <n.rabone72@aol.com>
2025-09-23 19:40:43 +0100

    Simple variables

--- BasicTypes.hs
+++ BasicTypes.hs
@@ -6,3 +6,17 @@ main = do
   putStrLn "Enter your name: "
   name <- getLine
   putStrLn(greet name)
+
+person :: String
+person = "Nick"
+age :: Int
+age = 43
+piApprox :: Float
+piApprox = 3.14
+gravity :: Double
+gravity = 0.90665
+isWizard :: Bool
+isWizard = False
+
+
+
