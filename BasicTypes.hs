4f80901f8730329efedc0194376237031f38e610
Nimira43 <n.rabone72@aol.com>
2025-09-23 20:03:23 +0100

    Subtraction, multiplying and tweo ways to divide.

--- BasicTypes.hs
+++ BasicTypes.hs
@@ -4,6 +4,18 @@ greet name = "Welcome, " ++ name ++ ", to Haskell."
 add :: Int -> Int -> Int
 add x y = x + y
 
+sub ::  Int -> Int -> Int
+sub x y = x - y
+
+mult ::  Int -> Int -> Int
+mult x y = x * y
+
+divide ::  Int -> Int -> Int
+divide x y = x `div` y
+
+divide2 ::  Double -> Double -> Double
+divide2 x y = x / y
+
 person :: String
 person = "Nick"
 
@@ -25,8 +37,20 @@ main = do
   name <- getLine
   putStrLn(greet name)
   
-  let result = add 4 8
-  putStrLn ("The result is: " ++ show result)
+  let adding = add 4 8
+  putStrLn ("The result is: " ++ show adding)
+  
+  let deleting = sub 14 8
+  putStrLn ("The result is: " ++ show deleting)
+  
+  let multiplying = mult 4 5
+  putStrLn ("The result is: " ++ show multiplying)
+
+  let dividing = divide 15 5
+  putStrLn ("The result is: " ++ show dividing)
+  
+  let dividing2 = divide2 22 7
+  putStrLn ("The result is: " ++ show dividing2)
 
   putStrLn ("Person: " ++ person)
   putStrLn ("Age: " ++ show age)
