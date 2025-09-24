e5610123eb68c9b54df8837a27c49a294649fd97
Nimira43 <n.rabone72@aol.com>
2025-09-24 09:03:54 +0100

    Exact Rational Representation

--- DataTypes.hs
+++ DataTypes.hs
@@ -1,6 +1,7 @@
 import Data.List
 import System.IO
 import Text.Printf
+import Data.Ratio ((%))
 
 maxInt :: Int
 maxInt = maxBound :: Int
@@ -8,6 +9,9 @@ minInt :: Int
 minInt = minBound :: Int
 bigFloat :: Double
 bigFloat = 3.999999999999 + 0.000000000005
+exact :: Rational
+exact = 3999999999999 % 1000000000000 + 5 % 1000000000000
+
 
 main :: IO ()
 main = do
@@ -15,3 +19,5 @@ main = do
   putStrLn ("Min Integer using minBound: " ++ show minInt)
   putStrLn ("Floating-point Representation Demo: " ++ show bigFloat)
   printf "Floating-point with precision: %.18f\n" bigFloat
+  print exact
+  putStrLn ("Exact Rational Representation: " ++ show exact)
