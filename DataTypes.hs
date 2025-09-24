1123057c5eb3890fc07302dce6923751cd9ece68
Nimira43 <n.rabone72@aol.com>
2025-09-24 08:47:14 +0100

    minBound and maxBound

--- DataTypes.hs
+++ DataTypes.hs
@@ -1,3 +1,10 @@
 import Data.List
 import System.IO
 
+maxInt = maxBound :: Int
+minInt = minBound :: Int
+
+main :: IO ()
+main = do
+  putStrLn ("Max Integer using maxBound: " ++ show maxInt)
+  putStrLn ("Min Integer using minBound: " ++ show minInt)
