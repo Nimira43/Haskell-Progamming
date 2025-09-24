d8b2d0011c30ab95588b6f46d87075aa303d5d59
Nimira43 <n.rabone72@aol.com>
2025-09-24 08:54:17 +0100

    Floating-point Representation

--- DataTypes.hs
+++ DataTypes.hs
@@ -1,10 +1,15 @@
 import Data.List
 import System.IO
 
+maxInt :: Int
 maxInt = maxBound :: Int
+minInt :: Int
 minInt = minBound :: Int
+bigFloat :: Double
+bigFloat = 3.999999999999 + 0.000000000005
 
 main :: IO ()
 main = do
   putStrLn ("Max Integer using maxBound: " ++ show maxInt)
   putStrLn ("Min Integer using minBound: " ++ show minInt)
+  putStrLn ("Floating-point Representation Demo: " ++ show bigFloat)
