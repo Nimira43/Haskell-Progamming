1169202a6b3f39e0684691dbb82cbe5c9389870f
Nimira43 <n.rabone72@aol.com>
2025-09-25 02:52:32 +0100

    Quick tidy up

--- Cards.hs
+++ Cards.hs
@@ -6,7 +6,8 @@ newCard = "Five of Diamonds"
 main :: IO ()
 main = do
   let cards = ["Ace of Diamonds", newCard]
-      cards' = cards ++ ["Six of Spaces"] -- ' indicates modified cards
+      cards' = cards ++ ["Six of Spaces"] 
+      -- ' indicates modified cards
 
   mapM_ (\(i, card) -> putStrLn $ show i ++ " " ++ card) (zip [0..] cards')
 
@@ -14,13 +15,12 @@ main = do
   \(i, card) -> putStrLn $ show i ++ " " ++ card
 
   1) This is a lambda function — an anonymous spell.
-
   2) It takes a tuple (i, card) and prints it like: 0 Ace of Diamonds
-
   3) show i converts the index to a string.
-
   4) ++ is string concatenation.
-  
+
+  zip [0..] cards'
+
   5) zip pairs the card with it's index
 -}
    
