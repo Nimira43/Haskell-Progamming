c4ec224178ded6fb174a3d5c2038fbf44c13370d
Nimira43 <n.rabone72@aol.com>
2025-10-11 01:44:11 +0100

    map factorial

--- Program005.hs
+++ Program005.hs
@@ -7,3 +7,6 @@ main :: IO ()
 main = do
   let result = factorial 5
   putStrLn ("Factorial 5: " ++ show result)
+
+  let results = map factorial [0..5]
+  putStrLn ("Factorials from 0 to 5: " ++ show results)
