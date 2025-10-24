2a429ff8ca1be601c516c156b3de1330fda95c97
Nimira43 <n.rabone72@aol.com>
2025-10-24 04:44:28 +0100

    Declaring cards

--- Program006.hs
+++ Program006.hs
@@ -14,3 +14,7 @@ describeCard :: (rank, suit) = "The card is the " ++ rank ++ " of " ++ suit
 
 main :: IO ()
 main = do
+  let card1 = ("Seven", "Diamonds")
+      card2 = ("Ace", "Spades")
+      card3 = ("Nine", "Clubs")
+      card4 = ("Eight", "Hearts")
