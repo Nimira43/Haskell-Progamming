ac1b97215c33327c7f4ce4ce3d6be824a765359c
Nimira43 <n.rabone72@aol.com>
2025-11-11 21:16:52 +0000

    Update Rank declaration

--- Program007.hs
+++ Program007.hs
@@ -1,7 +1,8 @@
 -- Exploring Pattern Matching and Guards
 
 data Suit = Hearts | Diamonds | Clubs | Spades deriving Show
-data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving Show
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
+  deriving (Show, Eq, Enum)
 data Card = Card Rank Suit deriving Show
 
 describeCard :: Card -> String
