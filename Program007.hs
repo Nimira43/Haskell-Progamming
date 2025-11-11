f09eb2f309f8fd19e9d6484ea03ad4e136a5e00b
Nimira43 <n.rabone72@aol.com>
2025-11-11 20:51:37 +0000

    Define Card type uing Rank and Suit

--- Program007.hs
+++ Program007.hs
@@ -2,5 +2,5 @@
 
 data Suit = Hearts | Diamonds | Clubs | Spades deriving Show
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving Show
-
+data Card = Card Rank Suit deriving Show
 
