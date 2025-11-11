ad16ab94834279d352379c59e4595014f19ff360
Nimira43 <n.rabone72@aol.com>
2025-11-11 20:57:45 +0000

    Pattern matching with card

--- Program007.hs
+++ Program007.hs
@@ -5,3 +5,6 @@ data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack 
 data Card = Card Rank Suit deriving Show
 
 describeCard :: Card -> String
+describeCard (Card Ace Spades) = "The Ace of Spades"
+describeCard (Card King Hearts) = "The King of Hearts"
+describeCard (Card rank suit) = "This card is the " ++ show rank ++ " of " ++ show suit
