792fc81dd35e7c926417ee1622434d9f9b40cb8a
Nimira43 <n.rabone72@aol.com>
2025-11-11 20:53:12 +0000

    describeCard

--- Program007.hs
+++ Program007.hs
@@ -4,3 +4,4 @@ data Suit = Hearts | Diamonds | Clubs | Spades deriving Show
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving Show
 data Card = Card Rank Suit deriving Show
 
+describeCard :: Card -> String
