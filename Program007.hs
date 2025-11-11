9fae7e5dfc88dd2578c0e517844cc8b079f40686
Nimira43 <n.rabone72@aol.com>
2025-11-11 20:48:13 +0000

    Define a custom data type for rank

--- Program007.hs
+++ Program007.hs
@@ -2,4 +2,5 @@ import GHC.Generics (SourceUnpackedness)
 -- Exploring Pattern Matching and Guards
 
 data Suit = Hearts | Diamonds | Clubs | SourceUnpackedness
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving Show
 
