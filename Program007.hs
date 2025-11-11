c4d5292ad6c537ee1927513287d71e39bc3b4ec9
Nimira43 <n.rabone72@aol.com>
2025-11-11 20:50:48 +0000

    Corrected Suit.

--- Program007.hs
+++ Program007.hs
@@ -1,6 +1,6 @@
-import GHC.Generics (SourceUnpackedness)
 -- Exploring Pattern Matching and Guards
 
-data Suit = Hearts | Diamonds | Clubs | SourceUnpackedness
+data Suit = Hearts | Diamonds | Clubs | Spades deriving Show
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving Show
+
 
