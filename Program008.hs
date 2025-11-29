4d335353ff47a738f112de3121d0c600d8672a9d
Nimira43 <n.rabone72@aol.com>
2025-11-29 16:58:30 +0000

    Creating values

--- Program008.hs
+++ Program008.hs
@@ -1,6 +1,6 @@
 -- Algebraic Data Types
 
-data Suit = Heaarts | Diamonds | Clubs | Spades
+data Suit = Hearts | Diamonds | Clubs | Spades
 
 colour :: Suit -> String
 colour Hearts = "Red"
@@ -11,3 +11,6 @@ colour Spades = "Red"
 data Rank  = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
 
 data Card = Card Rank Suit
+
+myCard :: Card
+myCard = Card Seven Diamonds
