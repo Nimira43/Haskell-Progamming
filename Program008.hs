c26625619f051b12784132ede5f8bf90cde1bfc7
Nimira43 <n.rabone72@aol.com>
2025-11-29 17:06:21 +0000

    Functions. Also made corrections

--- Program008.hs
+++ Program008.hs
@@ -1,16 +1,27 @@
 -- Algebraic Data Types
 
 data Suit = Hearts | Diamonds | Clubs | Spades
+  deriving (Show)
 
 colour :: Suit -> String
 colour Hearts = "Red"
 colour Diamonds = "Red"
-colour Clubs = "Red"
-colour Spades = "Red"
+colour Clubs = "Black"
+colour Spades = "Black"
 
 data Rank  = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
+  deriving (Show)
 
 data Card = Card Rank Suit
-
+  deriving (Show)
+  
 myCard :: Card
 myCard = Card Seven Diamonds
+
+showCard :: Card -> String
+showCard (Card rank suit) = show rank ++ " of " ++ show suit
+
+main :: IO ()
+main = do
+  putStrLn (showCard myCard)
+  
