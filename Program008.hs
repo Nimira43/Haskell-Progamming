a54903ca1f36093a1dbfd2ed567bc60b32fc2b3d
Nimira43 <n.rabone72@aol.com>
2025-11-29 17:25:33 +0000

    Shape ADT and Cat ADT

--- Program008.hs
+++ Program008.hs
@@ -9,7 +9,7 @@ colour Diamonds = "Red"
 colour Clubs = "Black"
 colour Spades = "Black"
 
-data Rank  = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
+data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
   deriving (Show)
 
 data Card = Card Rank Suit
@@ -21,14 +21,22 @@ myCard = Card Seven Diamonds
 showCard :: Card -> String
 showCard (Card rank suit) = show rank ++ " of " ++ show suit
 
-data Shape = Circle Float
-           | Rectangle Float Float
+data Shape
+  = Circle Float
+  | Rectangle Float Float
 
 area :: Shape -> Float
-area (Circle r)       = pi * r^2
-area (Rectangle w h)  = w * h
+area (Circle r) = pi * r ^ 2
+area (Rectangle w h) = w * h
+
+data Cat
+  = Lenny
+  | OtherCat String
+  | Hero String Int
+  deriving (Show)
+
+
 
 main :: IO ()
 main = do
   putStrLn (showCard myCard)
-  
