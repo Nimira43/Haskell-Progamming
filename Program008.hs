dc6dfa713972795dda111b80a3af2a9e651555b3
Nimira43 <n.rabone72@aol.com>
2025-11-29 16:54:32 +0000

    Pattern match on suits

--- Program008.hs
+++ Program008.hs
@@ -1,3 +1,9 @@
 -- Algebraic Data Types
 
 data suit = Heaarts | Diamonds | Clubs | Spades
+
+colour :: Suit -> String
+colour Hearts = "Red"
+colour Diamonds = "Red"
+colour Clubs = "Red"
+colour Spades = "Red"
