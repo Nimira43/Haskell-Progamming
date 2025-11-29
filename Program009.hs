6a182e6d9e98ae7dac714e3363aa6eb9e8072a27
Nimira43 <n.rabone72@aol.com>
2025-11-29 17:48:11 +0000

    Defining card and also now derving Eq and Enum

--- Program009.hs
+++ Program009.hs
@@ -1,1 +1,16 @@
 -- Pattern Matching + ADTs
+
+data Suit = Hearts | Diamonds | Clubs | Spades
+  deriving (Show, Eq)
+colour :: Suit -> String
+colour Hearts = "Red"
+colour Diamonds = "Red"
+colour Clubs = "Black"
+colour Spades = "Black"
+
+data Rank
+  = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+  deriving (Show, Eq, Enum)
+
+data Card = Card Rank Suit
+  deriving (Show, Eq)
