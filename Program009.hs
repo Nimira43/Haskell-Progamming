70752049059bf5a59fda2052976eed2623d5f030
Nimira43 <n.rabone72@aol.com>
2025-11-29 17:52:10 +0000

    Card values with guards

--- Program009.hs
+++ Program009.hs
@@ -1,5 +1,5 @@
 -- Pattern Matching + ADTs
-
+-- Defining Card 
 data Suit = Hearts | Diamonds | Clubs | Spades
   deriving (Show, Eq)
 colour :: Suit -> String
@@ -15,8 +15,18 @@ data Rank
 data Card = Card Rank Suit
   deriving (Show, Eq)
 
+-- Describe cards with pattern matching
 describeCard :: Card -> String
 
 describeCard (Card Nine Clubs) = "The Nine of Clubs"
 describeCard (Card Three Hearts) = "The Three of Hearts"
 describeCard (Card rank suit) = "This card is the " ++ show rank ++ " of " ++ show suit
+
+-- Card values with guards
+cardValue :: Rank -> Int
+cardValue rank
+  | rank == Ace = 11
+  | rank == King = 10
+  | rank == Queen = 10
+  | rank == Jack = 10
+  | otherwise = fromEnum rank + 2
