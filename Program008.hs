b0b5c6bf7a08f88a86e1ba8c4efe4cf5b496f6e5
Nimira43 <n.rabone72@aol.com>
2025-11-29 16:57:16 +0000

    Defined Rank. Card is now a compound type

--- Program008.hs
+++ Program008.hs
@@ -1,9 +1,13 @@
 -- Algebraic Data Types
 
-data suit = Heaarts | Diamonds | Clubs | Spades
+data Suit = Heaarts | Diamonds | Clubs | Spades
 
 colour :: Suit -> String
 colour Hearts = "Red"
 colour Diamonds = "Red"
 colour Clubs = "Red"
 colour Spades = "Red"
+
+data Rank  = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace 
+
+data Card = Card Rank Suit
